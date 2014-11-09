4c2ad1b31dbe96f006f859a6d0cc0629a24c7208
Daniel Yu <dcyu93@gmail.com>
2014-11-08 21:26:31 -0600

    gmaps added

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -21,11 +21,41 @@ class CheckpointsController < ApplicationController
 
   def show
     @checkpoint = Checkpoint.find(params[:id])
+    @geo = Geocoder.coordinates(@checkpoint.name)
 
-    @tweets = $twitter.search("to:testmaftuh #{@checkpoint.name}", result_type: "recent").take(5)
+    @tweets = $twitter.search("to:testmaftuh #{@checkpoint.name}", result_type: "recent")
 
-    @status = @tweets.first.text.include?("open")
-    @checkpoint.update(open: @status)
+    if @tweets.count > 0
+      @status = @tweets.first.text.include?("open")
+      @checkpoint.update(open: @status)
+
+      @open_tweets = @tweets.select{|tweet| tweet.text.include?("open")}
+      @closed_tweets = @tweets.select{|tweet| tweet.text.include?("closed")}
+
+      data_table = GoogleVisualr::DataTable.new
+
+      # Add Column Headers
+      data_table.new_column('string', 'Hour' )
+      data_table.new_column('number', 'Open')
+      data_table.new_column('number', 'Closed')
+
+      # Add Rows and Values
+      grouped_tweets = @tweets.group_by{|x| x.created_at.strftime("%Y-%m-%d-%H")} 
+
+      grouped_tweets.reverse_each do |tweets|
+        open_tweets = tweets.last.select{|tweet| tweet.text.include?("open")}
+        closed_tweets = tweets.last.select{|tweet| tweet.text.include?("closed")}
+
+        data_table.add_rows([[
+          (Time.parse(tweets.first) + tweets.first.split('-').last.to_i.hours).to_formatted_s(:long), open_tweets.count, closed_tweets.count
+        ]]
+        )
+      end
+    end
+
+    option = { width: 400, height: 200, title: 'Recent Status Updates', colors: ['#009900', '#990000'] }
+    @chart = GoogleVisualr::Interactive::ColumnChart.new(data_table, option)
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Maftuh::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'checkpoints#index'
 
   resources :checkpoints
 
