d19fc1fe0ff00a963e3c3ffe5cdc4641b28d6dd1
Shaan Sapra <ssapra@uchicago.edu>
2014-11-10 14:49:59 -0600

    Fixing bugs in maps script and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ ar = File.readlines('checkpoints_ar.txt')
 combined = en.zip ar
 
 combined.each do |line|
-  c = Checkpoint.create name: line.first, ar: line.last, open: false
+  c = Checkpoint.create name: line.first.chomp, ar: line.last.chomp, open: false
   Message.create(checkpoint_id: c.id, body: "#{line.first} is closed")
   Message.create(checkpoint_id: c.id, body: "#{line.first} is open")
   Message.create(checkpoint_id: c.id, body: "#{line.first} is closed")
