f75c51a697beea2166396153d1a276355a7fd99b
Daniel Yu <dcyu93@gmail.com>
2014-11-10 00:38:08 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@ Message.create(checkpoint_id: c1.id, body: "Chicago is open")
 c2 = Checkpoint.create(name: "New York", open: false)
 Message.create(checkpoint_id: c2.id, body: "New York is false")
 Message.create(checkpoint_id: c2.id, body: "New York is open")
+
+
+en = File.readlines('checkpoints.txt')
+ar = File.readlines('checkpoints_ar.txt')
+
+combined = en.zip ar
+
+combined.each do |line|
+  Checkpoint.create name: line.first, ar: line.last, open: false
+end
