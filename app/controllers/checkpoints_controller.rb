106900ca631f89ef3de613ba47af559a0d1df14c
Daniel Yu <dcyu93@gmail.com>
2014-11-11 00:31:26 -0600

    changed tweet id.to_s

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -33,7 +33,7 @@ class CheckpointsController < ApplicationController
       m.body = tweet.text
       m.user = tweet.user.screen_name
       m.checkpoint_id = @checkpoint.id
-      m.tweet_id = tweet.id_str
+      m.tweet_id = tweet.id.to_s
       m.save
     end
 
