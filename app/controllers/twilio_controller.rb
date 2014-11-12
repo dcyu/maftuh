5911549d95f4aa9ac8d3dceb0f47fdf16b056fa0
Andrew Bourne <Andrew@Andrew-Bournes-MacBook-Pro-4.local>
2014-11-11 22:45:54 -0600

    wrote code for twilio response, added weighting to recent messages about status

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -32,32 +32,59 @@ class TwilioController < ApplicationController
         message = Message.create(body: body, checkpoint_id: checkpoint.id)        
       end
 
-      if checkpoints_ar.present?
-        @message = "شكرا لرسالتك. يتم تحديث وضع حاجز #{checkpoints.first.ar}."
-      else
-        @message = "Thanks for your message. The #{checkpoints.first.name} checkpoint status has been updated"
+      if is_question || ( !is_open && !is_closed )
+        @message = ""
+        checkpoints.each do |checkpoint|
+          if checkpoints_ar.present?
+            if checkpoint.open
+              @message += " ،مفتوح " + checkpoint.ar 
+            else
+              @message += " ،مغلق " + checkpoint.ar
+            end
+          else
+            if checkpoint.open
+              @message += checkpoint.name + " is open, "
+            else
+              @message += checkpoint.name + " is closed, "
+            end
+          end
+        end
+        #removes ", " substring
+        if checkpoints_ar.present?
+          @message = @message[2..-1]
+        else
+          @message = @message[0..-3]
+        end
+      elsif is_open || is_closed
+        checkpoint = checkpoints.first
+        #right now this query includes messages that do not report open or closed
+        recent_messages = Message.where(checkpoint_id: checkpoint.id).limit(100)
+        update_messages = recent_messages.select{ |m| m.body.downcase.include?(I18n.t('closed')) || m.body.downcase.include?(I18n.t('open'))}.sort_by{|m| m.created_at}.reverse[0..20]
+
+        # weights messages by log time since they were received
+        open_weight = 0
+        closed_weight = 0
+        update_messages.each do |message|
+          weight = 1 / (Math.log(Time.now - message.created_at) ** 2)
+          if message.body.downcase.include?(I18n.t('open'))
+            open_weight += weight
+          elsif message.body.downcase.include?(I18n.t('closed'))
+            closed_weight += weight
+          end
+        end
+
+        if open_weight >= closed_weight
+          checkpoint.update(open: true)
+        else
+          checkpoint.update(open: false)
+        end
+
+        if checkpoints_ar.present?
+          @message = "شكرا لرسالتك. يتم تحديث وضع حاجز #{checkpoints.first.ar}."
+        else
+          @message = "Thanks for your message. The #{checkpoints.first.name} checkpoint status has been updated"
+        end
       end
-      
-      # if is_question || ( !is_open && !is_closed )
-      #   @message = ""
-      #   checkpoints.each do |checkpoint|
-      #     if checkpoint.open
-      #       @message += I18n.t('is_open', name: checkpoint.name)
-      #     else
-      #       @message += I18n.t('is_closed', name: checkpoint.name)
-      #     end
-      #   end
-      #   #removes last ", " string
-      #   @message = @message[0..-3]
-      # elsif is_open || is_closed
-      #   Rails.logger.debug "#{checkpoints.size} checkpoints found"
-      #   checkpoints.each do |checkpoint|
-      #     checkpoint.update(open: status)
-      #     message = Message.new(body: body, checkpoint_id: checkpoint.id)
-      #     message.save!
-      #   end
-      #   @message = I18n.t('status_update_response', name: checkpoints.first.name)
-      # end
     end
     twiml = Twilio::TwiML::Response.new { |r|  r.Message @message }
     render text: twiml.text
